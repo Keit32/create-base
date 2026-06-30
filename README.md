a183e3fca11788b13b27c5690f6d17a2df8224b1
Keit <84858268+Keit32@users.noreply.github.com>
2026-06-30 22:56:16 +0500

    chore: project initialization

---
+++ README.md
@@ -0,0 +1,1 @@
+# @keit/create-base
---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "chore", "docs", "refactor", "test", "perf", "ci", "style", "revert"],
+    ],
+    "subject-case": [0],
+    "header-max-length": [2, "always", 100],
+  },
+};
